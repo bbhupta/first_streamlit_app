2f908544c760d30a83ee818a46035e024c445d53
bbhupta <bbhupta@gmail.com>
2022-04-29 16:38:33 +0530

    Update streamlit_app.py
    Pandas lib added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas as pd
 
 streamlit.title("My Parents New Healthy Diner")
 
@@ -7,3 +8,7 @@ streamlit.text("🥣 Omega 3 Blueberry Oatmeal")
 streamlit.text("🥗 Kale, Spinach & Rocket Smoothie")
 streamlit.text("🐔 Hard-Boiled Free-Range Egg")
 streamlit.text("🥑 🍞 Avocado Toast")
+
+streamlit.title("🍇 🥝 Build your Own Fruit Smoothie 🍌 🍍")
+my_fruit_list=pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
